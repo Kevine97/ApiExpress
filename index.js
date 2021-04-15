36c55c20de42bc0650b5079d230d50a67b58c606
Kevin Esquivel <kevinesquivelrojas@hotmail.com>
2021-04-15 09:17:27 -0600

    Habilitando cors 15/04/2021

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from "express";
 import conectarDB from "./config/db.js";
 import router from "./routes/route.js";
+import cors from "cors";
+
 
 //Crear el servidor
 const app = express();
+
+const whitelist = ["http://localhost:4000",'http://localhost:5500'];
+
+const corsOptions = {
+  
+  origin: (origin, callback) => {
+    console.log(origin);
+    const existe = whitelist.some((dominio) => dominio === origin);
+
+    if (existe) {
+      callback(null, true);
+    } else {
+      callback(new Error("No tiene permiso para utlizar esta API REST"));
+    }
+  },
+};
+
+//Habilitar cors
+app.use( cors(corsOptions) );
 
 //conectar a la DBR
 conectarDB();
